0c0bad3c20c81b546e0b63ebdb54844eab260c83
Guilherme Costa <gui-costaquino@hotmail.com>
2023-11-02 16:58:27 -0300

    Adicionando mais dados ao arquivo

--- data.ts
+++ data.ts
@@ -12,4 +12,46 @@ export const cards = [
     description: "O Congresso da Sociedade Brasileira de Computação",
     date: "06 a 11 de Agosto",
   },
+  {
+    img: "./src/assets/img/foto.png",
+    tittle: "IV SertãoComp",
+    description:
+      "4° edição do SertãoComp, acontecerá presencialmente no campus Cajazeiras.",
+    date: "04 a 07 de Outubro",
+  },
+  {
+    img: "./src/assets/img/foto.png",
+    tittle: "IV SertãoComp",
+    description:
+      "4° edição do SertãoComp, acontecerá presencialmente no campus Cajazeiras.",
+    date: "04 a 07 de Outubro",
+  },
+  {
+    img: "./src/assets/img/foto.png",
+    tittle: "IV SertãoComp",
+    description:
+      "4° edição do SertãoComp, acontecerá presencialmente no campus Cajazeiras.",
+    date: "04 a 07 de Outubro",
+  },
+  {
+    img: "./src/assets/img/foto.png",
+    tittle: "IV SertãoComp",
+    description:
+      "4° edição do SertãoComp, acontecerá presencialmente no campus Cajazeiras.",
+    date: "04 a 07 de Outubro",
+  },
+  {
+    img: "./src/assets/img/foto.png",
+    tittle: "IV SertãoComp",
+    description:
+      "4° edição do SertãoComp, acontecerá presencialmente no campus Cajazeiras.",
+    date: "04 a 07 de Outubro",
+  },
+  {
+    img: "./src/assets/img/foto.png",
+    tittle: "IV SertãoComp",
+    description:
+      "4° edição do SertãoComp, acontecerá presencialmente no campus Cajazeiras.",
+    date: "04 a 07 de Outubro",
+  },
 ];
