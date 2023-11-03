df5075793ae5bbabc8bb519b03673e01cc914194
Guilherme Costa <gui-costaquino@hotmail.com>
2023-11-03 14:53:56 -0300

    Adicionando mais dados

--- data.ts
+++ data.ts
@@ -1,10 +1,14 @@
 export const cards = [
   {
     img: "./src/assets/img/sertaoComp.png",
+    img2: "./src/assets/img/sertaoComp2.png",
     tittle: "IV SertãoComp",
     description:
       "4° edição do SertãoComp, acontecerá presencialmente no campus Cajazeiras.",
-    date: "04 a 07 de Outubro",
+    description2:
+      "O IV SertãoComp será uma grande oportunidade de reunir profissionais, acadêmicos e alunos dos cursos superiores, ligados à ciência e tecnologia do Sertão Nordestino, para discutir tópicos relevantes e atuais da Ciência da Computação. Outrossim, uma vitrine para empresas garimparem novos talentos, prospectarem oportunidades na região, além de poderem discutir qual o melhor perfil do profissional ligado à informática que é requerido para atuar no mercado do trabalho atual.",
+    date: "04 a 07 de Outubro de 2023",
+    location: "IFPB Campus Cajazeiras",
   },
   {
     img: "./src/assets/img/csbc.png",
