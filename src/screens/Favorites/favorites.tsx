ae896b9da6ebae74c50fb032518ab6d9acf6bf19
leomendes18 <leomendesmonteiro1@gmail.com>
2023-12-17 22:56:23 -0300

    implementando função de procurar Favoritos

--- src/screens/Favorites/favorites.tsx
+++ src/screens/Favorites/favorites.tsx
@@ -2,8 +2,39 @@ import "./favorites.css";
 import { Header } from "../../components/Header/header";
 import { Card } from "../../components/Card/card";
 import { cards } from "../../../data";
+import { api } from "../../utils/api";
+import { useState } from "react";
+
+type Events = {
+  id: string;
+  title: string;
+  picture: string;
+  description: string;
+  date: string;
+  theme: string;
+  location: string;
+};
 
 export function Favorites() {
+  const [favorites, setFavorites] = useState<Events[]>([]);
+  //const context = useAuth();
+
+  async function findFavorites() {
+    try {
+      api
+        .post("/getFavoriteList", { email: context.user?.email })
+        .then((res) => {
+          const list = res.data.favoriteList;
+          setFavorites(list);
+        });
+    } catch (error) {
+      console.log("ERRO: " + error);
+    }
+  }
+
+  useFocusEffect(() => {
+    findFavorites();
+  });
   return (
     <>
       <Header />
@@ -29,4 +60,7 @@ export function Favorites() {
       </div>
     </>
   );
+}
+function useFocusEffect(arg0: () => void) {
+  throw new Error("Function not implemented.");
 }
