7a83d6f2cea3804f2f118d196ac801d40cc431e4
AlissonAlmeidaSS <alisson.ifpb@gmail.com>
2024-09-26 10:02:44 -0300

    adicionando evento

--- src/Router.tsx
+++ src/Router.tsx
@@ -9,6 +9,7 @@ import { Events } from "./screens/Events/events";
 import { Favorites } from "./screens/Favorites/favorites";
 import { Participating } from "./screens/Participating/participating";
 import { DetailEvent } from "./screens/DetailEvent/detailEvent";
+import { criarEvents } from "./screens/CriarEvents/criarEvents";
 
 export function Router() {
   const context = useAuth();
@@ -26,6 +27,7 @@ export function Router() {
           <Route path="/" element={<Profile />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/editProfile" element={<EditProfile/>} />
+          <Route path="/createEvents" element={<criarEvents/>} />
           <Route path="/events" element={<Events />} />
           <Route path="/favorites" element={<Favorites />} />
           <Route path="/participating" element={<Participating />} />
---
+++ src/screens/CriarEvents/criarEvents.tsx
@@ -0,0 +1,49 @@
+import { Header } from "../../components/Header/header";
+import "./events.css";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import { date } from "yup";
+
+type Events = {
+  _id: string;
+  title: string;
+  picture: string;
+  description: string;
+  date: string;
+  theme: string;
+  location: string;
+};
+
+export function criarEvents(){
+  return (
+    <div id="bodyPageCriarEvento">
+      <Header />
+      <div className="body">
+        <div className="boxCriarEvento">
+          <div>
+            <label for="title">Título do evento <span>(Minimo 8 caracteres)</span></label>
+            <input type="text" id="title" required placeholder=""/>
+          </div>
+          
+          <div>
+            <img src={description} alt="description" className="description" />
+            <button>
+              <input type="text" id="description" name="description" />
+            </button>
+          </div>
+
+          <div>
+            <button>
+              <input type="date" id="start" name="trip-start" value="2018-07-22" min="2018-01-01" max="2018-12-31" />
+            </button>
+          </div>
+
+        </div>  
+      </div>;
+    </div>
+
+  )
+}
+
